f55ba1b68115831da1237bfb4a7db9ed3bd42885
UrunoRikuto <nagito2314@gmail.com>
2025-11-20 04:35:33 +0900

    Fix: カメラ操作の統一化

--- MyProject_SimulationGame/CameraDebug.cpp
+++ MyProject_SimulationGame/CameraDebug.cpp
@@ -1,6 +1,7 @@
 #include "CameraDebug.h"
 #include "Input.h"
 #include "Oparation.h"
+#include "Main.h"
 
 constexpr float ce_fCameraRotate(0.001f);
 
@@ -38,47 +39,22 @@ void CCameraDebug::Update()
 
 	// キーボード入力からVelocityを計算
 	DirectX::XMFLOAT3 f3Velocity{};
-	if (IsKeyPress('W'))f3Velocity += f3Forward;
-	if (IsKeyPress('S'))f3Velocity -= f3Forward;
+	if (IsMouseWheelUp())f3Velocity += f3Forward;
+	if (IsMouseWheelDown())f3Velocity -= f3Forward;
 	if (IsKeyPress('D'))f3Velocity += f3Right;
 	if (IsKeyPress('A'))f3Velocity -= f3Right;
-	if (IsKeyPress('Q'))f3Velocity += fUp;
-	if (IsKeyPress('E'))f3Velocity -= fUp;
+	if (IsKeyPress(VK_SPACE))f3Velocity += fUp;
+	if (IsKeyPress(VK_SHIFT))f3Velocity -= fUp;
+	if (IsKeyPress('W'))m_fRadY += 0.01f;
+	if (IsKeyPress('S'))m_fRadY -= 0.01f;
+	if (IsKeyPress('E'))m_fRadXZ += 0.01f;
+	if (IsKeyPress('Q'))m_fRadXZ -= 0.01f;
 
 	// 計算したVelocityを注視点に加算
 	m_f3Look += f3Velocity;
 
-	// 右クリックした際にカーソル表示を無効化し、マウスの座標を中央に移す
-	if (IsMouseButtonTrigger(MOUSEBUTTON_R))
-	{
-		POINT center;
-		center.x = 0;
-		center.y = 0;
-		ShowCursor(false);
-		SetMousePosition(center);
-	}
-
-	// 右クリックを押しながらカメラの回転を行う
-	if (IsMouseButtonPress(MOUSEBUTTON_R))
-	{
-		// マウス座標から中央からの移動量を取得する
-		POINT mousePos = *GetMousePosition();
-		// 移動量を回転に反映させる
-		m_fRadXZ += mousePos.x * ce_fCameraRotate;
-		m_fRadY -= mousePos.y * ce_fCameraRotate;
-
-		// カメラの座標を中央に戻す
-		POINT center;
-		center.x = 0;
-		center.y = 0;
-		SetMousePosition(center);
-	}
-
-	// 右クリックを離した際にカーソル表示をもとに戻す
-	if (IsMouseButtonRelease(MOUSEBUTTON_R))
-	{
-		ShowCursor(true);
-	}
+	// マウスホイールの回転量をリセット
+	ResetMouseWheelDelta();
 
 	// 注視点、回転、カメラの距離からカメラの座標を計算する
 	m_f3Pos.x = cosf(m_fRadY) * sinf(m_fRadXZ) * m_fRadius + m_f3Look.x;
